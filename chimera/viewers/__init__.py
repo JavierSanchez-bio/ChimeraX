e0bfbab108ac8d20d4bc4b429bef3bfd3ad835e9
Javier Sanchez <javisanchezdelrio@gmail.com>
2025-05-10 13:18:13 +0200

    Backup ChimeraX plugin with new protocol

---
+++ chimera/viewers/__init__.py
@@ -0,0 +1,11 @@
+from .viewer import (ChimeraProtRigidFitViewer,
+                     ChimeraProtOperateViewer,
+                     ChimeraRestoreViewer,
+                     ChimeraModelFromTemplateViewer,
+                     ChimeraSubtractionMapsViewer,
+                     ChimeraAlphafoldViewer,
+                     PAEViewer,
+                     )
+from .viewer_contacts import ChimeraProtContactsViewer
+
+from .viewer_discrepancies import ChimeraProtDiscrepanciesViewer
